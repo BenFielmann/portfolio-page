80c55c2acd73a96c36c554733a32fc4c60bb4a59
Ben <benjamin-fielmann@gmx.de>
2023-09-23 14:28:35 +0200

    impliment text switch & add the z index to section switch

--- script.js
+++ script.js
@@ -18,9 +18,11 @@ function loadSelectedSection() {
     // Alle Abschnitte ausblenden
     sections.forEach(function (sec) {
       sec.style.opacity = "0";
+      sec.style.zIndex = "0"; // Setze den z-index für alle Abschnitte auf 0
     });
     // Zielabschnitt anzeigen
     sections[selectedIndex].style.opacity = "1";
+    sections[selectedIndex].style.zIndex = "1"; // Setze den z-index für den ausgewählten Abschnitt auf 1
   }
 }
 
@@ -35,9 +37,11 @@ for (var i = 2; i <= 6; i++) {
         // Alle Abschnitte ausblenden
         sections.forEach(function (sec) {
           sec.style.opacity = "0";
+          sec.style.zIndex = "0"; // Setze den z-index für alle Abschnitte auf 0
         });
         // Zielabschnitt anzeigen
         targetSection.style.opacity = "1";
+        targetSection.style.zIndex = "1"; // Setze den z-index für den ausgewählten Abschnitt auf 1
         // Aktiven Button markieren
         markActiveButton(index);
         // Ausgewählten Abschnitt in der SessionStorage speichern
@@ -52,9 +56,11 @@ document.getElementById("homeButton").addEventListener("click", function () {
   // Alle Abschnitte ausblenden
   sections.forEach(function (sec) {
     sec.style.opacity = "0";
+    sec.style.zIndex = "0"; // Setze den z-index für alle Abschnitte auf 0
   });
   // Startabschnitt anzeigen (Section 1)
   document.getElementById("section1").style.opacity = "1";
+  document.getElementById("section1").style.zIndex = "1"; // Setze den z-index für Section 1 auf 1
   // Aktiven Button markieren
   markActiveButton(0);
   // Ausgewählten Abschnitt in der SessionStorage löschen
@@ -63,6 +69,52 @@ document.getElementById("homeButton").addEventListener("click", function () {
 
 // Beim Laden der Seite den ausgewählten Abschnitt aus der SessionStorage laden
 loadSelectedSection();
+
+
+// ____________
+// // Home Section text switch
+
+// Ein Array mit den verschiedenen Texten
+var texts = [
+  "Junior Web Developer",
+  "Junior Web Designer",
+  "PC Hard & Software Nerd",
+  "Anime & Gaming fan",
+];
+var currentIndex = 0; // Der aktuelle Index im Array
+var textSwitchElement = document.getElementById("textSwitchElement"); // Das HTML-Element, in dem der Text angezeigt wird
+
+// Die Dauer des Breitenübergangs in Millisekunden
+var widthTransitionDuration = 2000; // Ändere diesen Wert nach Bedarf (z. B. 2000 für 2 Sekunden)
+
+function changeText() {
+  var text = texts[currentIndex]; // Den nächsten Text auswählen
+
+  // Ändere die Breite auf 100% mit der festgelegten Dauer
+  textSwitchElement.style.transition = `width ${widthTransitionDuration}ms ease-in-out`;
+  textSwitchElement.style.width = "100%"; // Breite auf 100% setzen (vollständig anzeigen)
+
+  // Nach einer kurzen Verzögerung den Text einblenden
+  setTimeout(function () {
+    textSwitchElement.textContent = text; // Text einblenden
+
+    // Nachdem der Text eingeblendet wurde, Breite auf 0 setzen (ausblenden)
+    setTimeout(function () {
+      textSwitchElement.style.transition = `width ${widthTransitionDuration}ms ease-in-out`;
+      textSwitchElement.style.width = "0"; // Breite auf 0 setzen
+    }, 3500); // 1 Sekunde Verzögerung nachdem der Text eingeblendet wurde
+  }, 100); // 1 Sekunde Verzögerung nachdem die Breite auf 100% gesetzt wurde
+
+  setTimeout(function () {
+    currentIndex = (currentIndex + 1) % texts.length; // Zum nächsten Text wechseln (zyklisch)
+    changeText(); // Nächsten Text anzeigen
+  }, 6000); // 4 Sekunden Verzögerung nachdem der Text vollständig angezeigt wurde
+}
+
+// Initialen Text setzen und den Wechselprozess starten
+changeText();
+
+// ____________
 
 // Funktion zur Markierung des aktiven Buttons
 function markActiveButton(index) {
